06371b646e643335430c87774326dfa23e86ea80
Pavan.D <128026613+dvpavan-kumar@users.noreply.github.com>
2023-08-29 17:33:27 +0530

    adding coming soon page

---
+++ coming-soon-pages/page0/script.js
@@ -0,0 +1,37 @@
+var countDownEndDate = "september 5, 2023 09:00:00";
+
+var endDate = new Date(countDownEndDate).getTime();
+
+var x = setInterval(function () {
+  var timeNow = new Date().getTime();
+
+  var timeLeft = endDate - timeNow;
+
+  var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+  if (days < 10) {
+    days = "0" + days;
+  }
+
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
+
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
+
+  if (seconds < 10) {
+    seconds = "0" + seconds;
+  }
+
+  if (timeLeft > 0) {
+    document.getElementById("cdD").innerHTML = days;
+    document.getElementById("cdH").innerHTML = hours;
+    document.getElementById("cdM").innerHTML = minutes;
+    document.getElementById("cdS").innerHTML = seconds;
+  }
+}, 1000);
