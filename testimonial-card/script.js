b4b0f4853570a9401533f3845ad9cd2eb1a0aab8
Pavan.D <dpavan.vkumar@gmail.com>
2023-09-15 00:38:04 +0530

    add Testimonial Card

--- testimonial-card/script.js
+++ testimonial-card/script.js
@@ -1,5 +1,5 @@
 const testimonialsContainer = document.querySelector('.testimonials-container')
-const testimonial = document.querySelector('.testimonial')
+const testimonial = document.querySelector('.content')
 const userImage = document.querySelector('.user-image')
 const username = document.querySelector('.username')
 const role = document.querySelector('.role')
@@ -10,22 +10,18 @@ const testimonials = [
     position: 'Marketing',
     photo:
       'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&s=707b9c33066bf8808c934c8ab394dff6',
-    text:
-      "I've worked with literally hundreds of HTML/CSS developers and I have to say the top spot goes to this guy. This guy is an amazing developer. He stresses on good, clean code and pays heed to the details. I love developers who respect each and every aspect of a throughly thought out design and do their best to put it in code. He goes over and beyond and transforms ART into PIXELS - without a glitch, every time.",
-  },
+      text: "I've worked with hundreds of HTML/CSS developers, but this guy stands out. His code is clean and detailed, transforming designs flawlessly. He's a true professional."  },
   {
     name: 'June Cha',
     position: 'Software Engineer',
     photo: 'https://randomuser.me/api/portraits/women/44.jpg',
-    text:
-      'This guy is an amazing frontend developer that delivered the task exactly how we need it, do your self a favor and hire him, you will not be disappointed by the work delivered. He will go the extra mile to make sure that you are happy with your project. I will surely work again with him!',
+    text: "An amazing frontend developer who delivers projects precisely as needed. Hiring him is a smart move; he goes the extra mile to ensure satisfaction. I'll definitely work with him again!"
   },
   {
     name: 'Iida Niskanen',
     position: 'Data Entry',
     photo: 'https://randomuser.me/api/portraits/women/68.jpg',
-    text:
-      "This guy is a hard worker. Communication was also very good with him and he was very responsive all the time, something not easy to find in many freelancers. We'll definitely repeat with him.",
+    text: "A hard worker with excellent communication skills. He's highly responsive, a rare quality among freelancers. We'll definitely work with him again."
   },
   {
     name: 'Renee Sims',
@@ -38,23 +34,20 @@ const testimonials = [
     name: 'Jonathan Nunfiez',
     position: 'Graphic Designer',
     photo: 'https://randomuser.me/api/portraits/men/43.jpg',
-    text:
-      "I had my concerns that due to a tight deadline this project can't be done. But this guy proved me wrong not only he delivered an outstanding work but he managed to deliver 1 day prior to the deadline. And when I asked for some revisions he made them in MINUTES. I'm looking forward to work with him again and I totally recommend him. Thanks again!",
+    text: "This guy is dedicated to getting the job done right. This is my second time hiring him, and I'll do it again in the future."
   },
   {
     name: 'Sasha Ho',
     position: 'Accountant',
     photo:
       'https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?h=350&auto=compress&cs=tinysrgb',
-    text:
-      'This guy is a top notch designer and front end developer. He communicates well, works fast and produces quality work. We have been lucky to work with him!',
+      text: "A top-notch designer and frontend developer. He communicates well, works quickly, and delivers quality work. We've been fortunate to work with him!"
   },
   {
     name: 'Veeti Seppanen',
     position: 'Director',
     photo: 'https://randomuser.me/api/portraits/men/97.jpg',
-    text:
-      'This guy is a young and talented IT professional, proactive and responsible, with a strong work ethic. He is very strong in PSD2HTML conversions and HTML/CSS technology. He is a quick learner, eager to learn new technologies. He is focused and has the good dynamics to achieve due dates and outstanding results.',
+    text: "A young and talented IT professional with a strong work ethic. He excels in PSD2HTML conversions and HTML/CSS technology. Quick to learn, he's focused and consistently meets deadlines with outstanding results."
   },
 ]
 
@@ -65,7 +58,7 @@ function updateTestimonial() {
 
   testimonial.innerHTML = text
   userImage.src = photo
-  username.innerHTML = name
+  username.innerHTML = name;
   role.innerHTML = position
 
   idx++
